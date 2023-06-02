4af94ba9d27006d25832995832f1a1500baf2ae6
Maksim Sheko <sakusan@mail.ru>
2023-06-03 01:03:51 +0300

    hw5 complete

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,6 +12,8 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+    const isActive = (isActive: { isActive: boolean }) => isActive.isActive ? s.active : ''
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +33,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive}  // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +49,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={isActive}  // делает студент
                     >
                         Junior Plus
                     </NavLink>
